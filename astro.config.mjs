ab7b2481d5ab153d89d0dde380b77ca5259380f9
hamb1y <rishi.s.malnad@gmail.com>
2025-07-26 15:34:50 +0530

    netlify is dumb(2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        'public/staticServe/images': path.resolve('public/images')
+      }
+    }
+  }
+});
+
