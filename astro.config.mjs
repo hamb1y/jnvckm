204018e57ef3b5ae1cd523566f2e87ca89bebd4b
hamb1y <rishi.s.malnad@gmail.com>
2025-07-28 17:07:19 +0530

    fixed title(1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import path from 'node:path';
 
 export default defineConfig({
+  site: "https://jnvckm.netlify.app", // 🔑 Required
+  integrations: [sitemap({
+    exclude: ["/admin/**", "/admin"]
+  })],
   vite: {
     resolve: {
       alias: {
@@ -9,5 +13,6 @@ export default defineConfig({
       }
     }
   }
+
 });
 
